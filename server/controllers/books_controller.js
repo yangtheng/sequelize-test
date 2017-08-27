cbbea2eb7b9c8c40963d6bf9b66dbde69a77d486
Rebecca Tay <rebeccatay92@gmail.com>
2017-08-27 08:33:13 +0800

    book controller can list and create now

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -1,9 +1,17 @@
 const Book = require('../models').Book
 
-function list (req,res) {
-  res.send("list fxn from booksController")
+function list (req, res) {
+  Book.findAll().then(books => {
+    res.send(books)
+  })
 }
 
+function create (req,res) {
+  Book.create({ title: '101 ways to eat a burger'}).then(book => {
+    res.send(book)
+  })
+}
 module.exports = {
-  list
+  list,
+  create
 }
--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const booksController = require('../controllers/books_controller')
 
 router.get('/', booksController.list)
+router.get('/create', booksController.create)
 
 module.exports = router
